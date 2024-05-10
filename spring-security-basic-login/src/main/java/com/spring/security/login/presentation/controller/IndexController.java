76f3dd3f518e15f53a18fac05cefdc99fccdb6f3
Min <>
2024-05-10 15:32:03 +0900

    다른 프로젝트들 컨트롤러 패키지 수정

--- spring-security-basic-login/src/main/java/com/spring/security/login/presentation/controller/IndexController.java
+++ spring-security-basic-login/src/main/java/com/spring/security/login/presentation/controller/IndexController.java
@@ -1,10 +1,11 @@
-package com.spring.security.login.controller;
+package com.spring.security.login.presentation.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+
     @GetMapping(value = "/index")
     public String index(){
         return "index";
--- spring-security-form-login/src/main/java/com/spring/security/login/presentation/controller/IndexController.java
+++ spring-security-form-login/src/main/java/com/spring/security/login/presentation/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.spring.security.login.controller;
+package com.spring.security.login.presentation.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
