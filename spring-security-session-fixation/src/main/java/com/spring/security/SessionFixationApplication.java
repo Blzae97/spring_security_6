088aafea4b24d934aa8b1380f322555004a30747
Min <>
2024-05-14 13:55:22 +0900

    스프링 시큐리티 세션 고정 보호 기능 추가

---
+++ spring-security-session-fixation/src/main/java/com/spring/security/SessionFixationApplication.java
@@ -0,0 +1,34 @@
+package com.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class SessionFixationApplication {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        String yml = getYml();
+        SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(SessionFixationApplication.class);
+        springApplicationBuilder.properties(yml);
+
+        SpringApplication springApplication = springApplicationBuilder.build();
+        springApplication.run(args);
+    }
+
+    public static String getYml(){
+        return "spring.config.location=" + getApplicationYml() + getDatasourceYml();
+    }
+
+    public static String getApplicationYml(){
+        return "classpath:/application.yml;";
+    }
+
+    public static String getDatasourceYml(){
+        return "classpath:/datasource.yml";
+    }
+}
---
+++ spring-security-session-fixation/src/main/java/com/spring/security/session/presentation/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.spring.security.session.presentation.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping(value = "/")
+    public Authentication index(Authentication authentication){
+        return authentication;
+    }
+}
