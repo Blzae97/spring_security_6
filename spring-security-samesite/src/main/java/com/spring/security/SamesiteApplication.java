abe0db8bb76448e7a26f6d998395f094fe46e704
Min <>
2024-05-18 02:11:50 +0900

    스프링 시큐리티 Samesite 구성 추가

---
+++ spring-security-samesite/src/main/java/com/spring/security/SamesiteApplication.java
@@ -0,0 +1,33 @@
+package com.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class SamesiteApplication {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        String yml = getYml();
+        SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(SamesiteApplication.class);
+        springApplicationBuilder.properties(yml);
+
+        SpringApplication springApplication = springApplicationBuilder.build();
+        springApplication.run(args);
+    }
+
+    public static String getYml(){
+        return "spring.config.location=" + getApplicationYml() + getDatasourceYml();
+    }
+
+    public static String getApplicationYml(){
+        return "classpath:/application.yml;";
+    }
+
+    public static String getDatasourceYml(){
+        return "classpath:/datasource.yml";
+    }
+}
---
+++ spring-security-samesite/src/main/java/com/spring/security/samestie/infrastructure/config/SpringSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.spring.security.samestie.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                );
+
+        http
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
