1c71500693a020f75d33024d546c86c4886694b8
Min <>
2024-05-20 13:10:20 +0900

    스프링 시큐리티 CustomRequestMatcher 구성 추가

---
+++ spring-security-custom-requestmatcher/src/main/java/com/spring/security/custom/infrastructure/config/CustomRequestMatcher.java
@@ -0,0 +1,20 @@
+package com.spring.security.custom.infrastructure.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+
+public class CustomRequestMatcher implements RequestMatcher {
+    private final String urlPattern;
+
+    public CustomRequestMatcher(String urlPattern) {
+        this.urlPattern = urlPattern;
+    }
+
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+
+        return requestURI.startsWith(urlPattern);
+    }
+}
