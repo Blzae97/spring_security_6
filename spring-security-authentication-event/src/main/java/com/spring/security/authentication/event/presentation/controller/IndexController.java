9fe031977aa90d2e2d627142844e67291dccacb2
Min <>
2024-06-11 17:27:17 +0900

    Implement: add authentication event
    add default authentication event example module

---
+++ spring-security-authentication-event/src/main/java/com/spring/security/authentication/event/presentation/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.spring.security.authentication.event.presentation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/db")
+    public String db() {
+        return "db";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
