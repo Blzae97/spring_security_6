c59b14955ecdb7ccb73d46c13342f231fa5bddb5
Min <>
2024-06-12 18:47:55 +0900

    Implement: add redis session login feature
    add redis session login example module

---
+++ spring-security-redis-session/src/main/java/com/spring/security/presentation/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.spring.security.presentation.controller;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+    @GetMapping("/db")
+    public String db(){
+        return "db";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+}
